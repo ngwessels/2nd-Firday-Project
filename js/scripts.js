c8e863759acc21ad5b0d842d46696f37ed90d6b0
Nate Wessels <nwessels16@gmail.com>
2019-01-18 09:18:29 -0800

    gave all forms a id instead of class

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+debugger;
+
+function answer1(answer) {
+
+}
+
+$(document).ready(function() {
+  $("form#formOne").submit(function() {
+    var firstAnswer = $("#firstAnswer").val();
+    
+
+
+
+
+
+    event.preventDefault();
+  });
+
+
+
+
+
+
+
+
+
+
+
+});
